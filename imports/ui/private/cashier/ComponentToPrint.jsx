aa4cb06174f02df67ccaf94cdbe731c981241203
unknown <Administrator@100-NSTAPPS-03.bankmestika.co.id>
2023-01-17 18:04:22 +0700

    test edit print

--- imports/ui/private/cashier/ComponentToPrint.jsx
+++ imports/ui/private/cashier/ComponentToPrint.jsx
@@ -4,33 +4,41 @@ import * as React from 'react';
 // the node you want to be the root of the print (usually the outer most node in the ComponentToPrint)
 // https://reactjs.org/docs/refs-and-the-dom.html#refs-and-function-components
 export const ComponentToPrint = React.forwardRef((props, ref) => {
+
   return (
     <div ref={ref}>
-      
-      <table>
+      <div style={{ width:280, margin:8}}>
+        <div className="text-center">Mencirim Swalayan</div>
+        <div className="text-center">{'( Cv. Mantap Agung Sejati )'}</div>
+        <div className="text-center">NPWP : 91.264640.3-125.000</div>
+        <div className="text-center">JL. SEI MENCIRIM DUSUN III</div>
+        <div >No.</div>
+        <div >dd-mm-yyyy</div>
+        <table>
           <thead>
-            <th>column 1</th>
-            <th>column 2</th>
-            <th>column 3</th>
+            <th>KodeBrg</th>
+            <th>NamaBarang</th>
           </thead>
           <tbody>
             <tr>
-              <td>data 1</td>
-              <td>data 2</td>
-              <td>data 3</td>
+              <td>00008013</td>
+              <td>GOLDEN NUMBER 111 BIRU BERAS 10</td>
+            </tr>
+            <tr>
+              <td>8</td>
+              <td>@141.750</td>
+              <td>1.134.0000</td>
             </tr>
             <tr>
               <td>data 1</td>
               <td>data 2</td>
-              <td>data 3</td>
-            </tr>
-            <tr>
-              <td>data 1</td>
-              <td>data 2</td>
-              <td>data 3</td>
             </tr>
           </tbody>
         </table>
+        <div className="text-center">BARANG YANG SUDAH DIBELI</div>
+        <div className="text-center">TIDAK DAPAT DITUKAR/DIKEMBALIKAN</div>
+        <div className="text-center">TERIMA KASIH</div>
+      </div>
     </div>
   );
 });
